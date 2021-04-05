b37f7e52329ad4edba0b78fbcae0f06ac8b3615c
abaksy <abaksy@gmail.com>
2021-04-05 19:28:56 +0530

    Minor refactoring of clean.py

--- clean.py
+++ clean.py
@@ -7,25 +7,24 @@ import re
 def load_fileslist():
     if "fileslist.pkl" not in os.listdir("."):
         files_list = list()
+        clean_files_list = list()
         for data_dir in os.listdir("./Data"):
             if data_dir.startswith("Year_"):
                 for fname in os.listdir(f"Data/{data_dir}"):
                     files_list.append(f"Data/{data_dir}/{fname}")
+                    clean_files_list.append(f"Clean_Data/{data_dir}/{fname}")
         filename = "fileslist.pkl"
         with open(filename, "wb") as f:
             pickle.dump(files_list, f)
+        clean_filename = "cleanfileslist.pkl"
+        with open(filename, "wb") as f:
+            pickle.dump(clean_files_list, f)
     else:
         with open("fileslist.pkl", "rb") as f:
             files_list = pickle.load(f)
-            f.close()
-    return files_list
-
-
-def load_cleanfileslist():
-    clean_files_list = list()
-    for fname in files_list:
-        clean_files_list.append("Clean_"+fname)
-    return clean_files_list
+        with open("cleanfileslist.pkl", "rb") as f:
+            clean_files_list = pickle.load(f)
+    return files_list, clean_files_list
 
 
 stop = stopwords.words('english')
@@ -51,8 +50,7 @@ def cleanText(text: str):
 
 
 if __name__ == "__main__":
-    files_list = load_fileslist()
-    clean_files_list = load_cleanfileslist()
+    files_list, clean_files_list = load_fileslist()
     if "Clean_Data" not in os.listdir("."):
         make_clean_dir()
     for fname, clean_fname in zip(files_list, clean_files_list):
